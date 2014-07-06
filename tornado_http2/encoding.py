fbb0b88ccd78bdc1bae380c011fae3028da56e28
Ben Darnell <bdarnell@squareup.com>
2014-07-06 17:05:56 -0400

    Implement hpack integer encoding.

--- tornado_http2/encoding.py
+++ tornado_http2/encoding.py
@@ -6,6 +6,9 @@ class BitEncoder(object):
         self._data = bytearray()
         self._bit_offset = 8
 
+    def data(self):
+        return self._data
+
     def write_bit(self, b):
         if self._bit_offset > 7:
             self._data.append(0)
@@ -13,8 +16,26 @@ class BitEncoder(object):
         self._data[-1] |= b << (7 - self._bit_offset)
         self._bit_offset += 1
 
-    def data(self):
-        return self._data
+    def write_hpack_int(self, i):
+        """Encodes an integer as defined by HPACK.
+
+        http://tools.ietf.org/html/draft-ietf-httpbis-header-compression-08#section-6.1
+        """
+        n = 8 - self._bit_offset
+        if n == 0:
+            self._data.append(0)
+            n = 8
+        if i < (1 << n) - 1:
+            self._data[-1] |= i
+            self._bit_offset = 8
+            return
+        self._data[-1] |= (1 << n) - 1
+        self._bit_offset = 8
+        i -= (1 << n) - 1
+        while i >= 128:
+            self._data.append((i & 127) + 128)
+            i >>= 7
+        self._data.append(i)
 
 class BitDecoder(object):
     def __init__(self, data):
@@ -37,3 +58,20 @@ class BitDecoder(object):
             self._byte_offset += 1
             self._bit_offset = 0
         return bit
+
+    def read_hpack_int(self):
+        prefix_mask = (1 << (8 - self._bit_offset)) - 1
+        i = self._data[self._byte_offset] & prefix_mask
+        self._bit_offset = 0
+        self._byte_offset += 1
+        if i < prefix_mask:
+            return i
+        m = 0
+        while True:
+            b = self._data[self._byte_offset]
+            self._byte_offset += 1
+            i += (b & 0x7f) << m
+            m += 7
+            if not (b & 0x80):
+                break
+        return i
--- tornado_http2/test/encoding_test.py
+++ tornado_http2/test/encoding_test.py
@@ -2,35 +2,93 @@ import unittest
 
 from tornado_http2.encoding import BitEncoder, BitDecoder, EODError
 
-class BitEncoderTest(unittest.TestCase):
-    def test_write_bit(self):
-        encoder = BitEncoder()
-        encoder.write_bit(1)
-        self.assertEqual(encoder.data(), b'\x80')
-        for b in [0, 1, 1, 0]:
-            encoder.write_bit(b)
-        self.assertEqual(encoder.data(), b'\xb0')
-        for b in [1, 1, 1]:
-            encoder.write_bit(b)
-        # We have now written 8 bits; the first byte is full and the second
-        # is not started.
-        self.assertEqual(encoder.data(), b'\xb7')
-        # Start the second byte.
-        encoder.write_bit(0)
-        self.assertEqual(encoder.data(), b'\xb7\x00')
-        for i in range(7):
-            encoder.write_bit(1)
-        self.assertEqual(encoder.data(), b'\xb7\x7f')
+class TestData(object):
+    def __init__(self, *args):
+        self.args = args
 
-class BitDecoderTest(unittest.TestCase):
-    def test_read_bit(self):
-        decoder = BitDecoder(bytearray(b'\xb7\x7f'))
-        self.assertEqual(decoder.read_bit(), 1)
-        # Finish the first byte.
-        for b in [0, 1, 1, 0, 1, 1, 1]:
-            self.assertEqual(decoder.read_bit(), b)
-        # Read the second byte.
-        for b in [0, 1, 1, 1, 1, 1, 1, 1]:
-            self.assertEqual(decoder.read_bit(), b)
-        with self.assertRaises(EODError):
-            decoder.read_bit()
+    def encode(self, encoder):
+        for arg in self.args:
+            self.encode_value(encoder, arg)
+
+    def decode(self, test, decoder):
+        for arg in self.args:
+            test.assertEqual(self.decode_value(decoder), arg)
+
+class Bits(TestData):
+    def encode_value(self, encoder, arg):
+        encoder.write_bit(arg)
+
+    def decode_value(self, decoder):
+        return decoder.read_bit()
+
+class HpackInt(TestData):
+    def encode_value(self, encoder, arg):
+        encoder.write_hpack_int(arg)
+
+    def decode_value(self, decoder):
+        return decoder.read_hpack_int()
+
+test_data = [
+    ('1-bit', [Bits(1)], [0b10000000], False),
+    ('5-bits', [Bits(1, 0, 1, 1, 0)], [0b10110000], False),
+    # 8 bits: the first byte is full and the second is not started.
+    ('8-bits', [Bits(1, 0, 1, 1, 0, 1, 1, 1)], [0b10110111], True),
+    ('9-bits', [Bits(1, 0, 1, 1, 0, 1, 1, 1, 0)],
+     [0b10110111, 0b00000000], False),
+    ('16-bits', [Bits(1, 0, 1, 1, 0, 1, 1, 1,
+                      0, 1, 1, 1, 1, 1, 1, 1)],
+     [0b10110111, 0b01111111], True),
+
+    # Test cases from
+    # http://tools.ietf.org/html/draft-ietf-httpbis-header-compression-08#appendix-D.1
+    # Encode 10 with a 5-bit prefix.
+    ('D.1.1', [Bits(1, 0, 1), HpackInt(10)], [0b10101010], True),
+    # Encode 1337 with a 5-bit prefix.
+    ('D.1.2', [Bits(0, 1, 0), HpackInt(1337)],
+     [0b01011111, 0b10011010, 0b00001010], True),
+    # Encode 42 on a byte boundary.
+    ('D.1.3', [HpackInt(42)], [42], True),
+
+    # Edge cases.
+    # Rollover from 1 byte to 2.
+    ('8-bit-prefix', [HpackInt(254)], [0b11111110], True),
+    ('8-bit-prefix2', [HpackInt(255)], [0b11111111, 0b00000000], True),
+    ('8-bit-prefix3', [HpackInt(256)], [0b11111111, 0b00000001], True),
+    # A single bit followed by a 7-bit prefix.
+    ('7-bit-prefix', [Bits(1), HpackInt(126)],
+     [0b11111110], True),
+    ('7-bit-prefix2', [Bits(1), HpackInt(127)],
+     [0b11111111, 0b00000000], True),
+    # Rollover from 2 bytes to 3.
+    ('3-byte-rollover', [HpackInt(382)], [0b11111111, 0b01111111], True),
+    ('3-byte-rollover2', [HpackInt(383)], [0b11111111, 0b10000000, 0b00000001],
+     True),
+    ]
+
+class BitEncodingTest(unittest.TestCase):
+    def test_bit_encoder(self):
+        for name, calls, data, complete in test_data:
+            try:
+                encoder = BitEncoder()
+                for c in calls:
+                    c.encode(encoder)
+                self.assertEqual(encoder.data(), bytearray(data))
+            except Exception:
+                print("Error in test case %s" % name)
+                raise
+
+    def test_bit_decoder(self):
+        for name, calls, data, complete in test_data:
+            try:
+                decoder = BitDecoder(bytearray(data))
+                for c in calls:
+                    c.decode(self, decoder)
+                if complete:
+                    self.assertRaises(EODError, decoder.read_bit)
+                else:
+                    decoder.read_bit()
+            except Exception:
+                print("Error in test case %s" % name)
+                print("Decoder offsets: %d, %d" % (
+                    decoder._byte_offset, decoder._bit_offset))
+                raise
