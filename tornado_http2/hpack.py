bc76f85dde4488f3fa0968e9a5e72563109de509
Ben Darnell <bdarnell@squareup.com>
2014-07-07 22:39:52 -0400

    Implement the reference set and differential encoding.

--- tornado_http2/hpack.py
+++ tornado_http2/hpack.py
@@ -1,38 +1,65 @@
+import collections
 import os
 from tornado.escape import utf8
 
-from .encoding import BitDecoder
+from .encoding import BitDecoder, EODError
 
 class HpackDecoder(object):
     def __init__(self):
-        # Dummy entry 0 in header_table
-        self._header_table = [None]
+        self._header_table = collections.deque()
         self._header_table_size = 0
+        # entries in the reference set are counted from the end of the
+        # header table.
+        self._reference_set = set()
 
     def decode(self, data):
         header_set = []
         bit_decoder = BitDecoder(data)
+        emitted_refs = set()
         while not bit_decoder.eod():
             is_indexed = bit_decoder.read_bit()
             if is_indexed:
                 idx = bit_decoder.read_hpack_int()
-                name, value = _static_table[idx]
+                ref_idx = len(self._header_table) - idx
+                if ref_idx in self._reference_set:
+                    self._reference_set.discard(ref_idx)
+                    continue
+                is_static, name, value = self.read_from_index(idx)
                 header_set.append((name, value))
-                self.add_to_header_table(name, value)
+                if is_static:
+                    self.add_to_header_table(name, value)
+                    ref_idx = len(self._header_table)
+                self._reference_set.add(ref_idx)
+                emitted_refs.add(ref_idx)
             else:
                 add_to_index = bit_decoder.read_bit()
                 if add_to_index:
                     name, value = self.read_name_value_pair(bit_decoder)
                     header_set.append((name, value))
                     self.add_to_header_table(name, value)
+                    ref_idx = len(self._header_table)
+                    self._reference_set.add(ref_idx)
+                    emitted_refs.add(ref_idx)
                 else:
                     is_context_update = bit_decoder.read_bit()
                     if is_context_update:
-                        raise NotImplementedError()
+                        clear_ref_set = bit_decoder.read_bit()
+                        new_limit = bit_decoder.read_hpack_int()
+                        if clear_ref_set:
+                            if new_limit != 0:
+                                raise ValueError(
+                                    "bits after clear_ref_set must be zero")
+                            self._reference_set.clear()
+                        else:
+                            raise NotImplementedError()
                     else:
                         # read the never-index bit and discard for now.
                         bit_decoder.read_bit()
                         header_set.append(self.read_name_value_pair(bit_decoder))
+        for ref_idx in self._reference_set - emitted_refs:
+            idx = len(self._header_table) - ref_idx + 1
+            _, name, value = self.read_from_index(idx)
+            header_set.append((name, value))
         return header_set
 
     def read_name_value_pair(self, bit_decoder):
@@ -40,7 +67,7 @@ class HpackDecoder(object):
         if name_index == 0:
             name = self.read_string(bit_decoder)
         else:
-            name = _static_table[name_index][0]
+            name = self.read_from_index(name_index)[1]
         value = self.read_string(bit_decoder)
         return name, value
 
@@ -48,13 +75,31 @@ class HpackDecoder(object):
         is_huffman = bit_decoder.read_bit()
         length = bit_decoder.read_hpack_int()
         if is_huffman:
-            read_char = bit_decoder.read_huffman_char
+            # read huffman chars until we have read 'length' bytes
+            dest_byte = bit_decoder._byte_offset + length
+            chars = []
+            while bit_decoder._byte_offset < dest_byte:
+                try:
+                    chars.append(bit_decoder.read_huffman_char())
+                except EODError:
+                    # TODO: fix handling of EOS char.
+                    break
+            while bit_decoder._bit_offset != 0:
+                pad_bit = bit_decoder.read_bit()
+                if not pad_bit:
+                    raise ValueError("padding bits must be 1")
         else:
-            read_char = bit_decoder.read_char
-        return bytes(bytearray([read_char() for i in range(length)]))
+            chars = [bit_decoder.read_char() for i in range(length)]
+        return bytes(bytearray(chars))
+
+    def read_from_index(self, idx):
+        if idx <= len(self._header_table):
+            return (False,) + self._header_table[idx - 1]
+        else:
+            return (True,) + _static_table[idx - len(self._header_table)]
 
     def add_to_header_table(self, name, value):
-        self._header_table.append((name, value))
+        self._header_table.appendleft((name, value))
         self._header_table_size += len(name) + len(value) + 32
 
 def _load_static_table():
--- tornado_http2/test/hpack_test.py
+++ tornado_http2/test/hpack_test.py
@@ -7,24 +7,90 @@ test_data = [
     # Test cases from
     # http://tools.ietf.org/html/draft-ietf-httpbis-header-compression-08#appendix-D.2
     ('D.2.1',
-     '400a 6375 7374 6f6d 2d6b 6579 0d63 7573'
-     '746f 6d2d 6865 6164 6572',
-     [(b'custom-key', b'custom-header')],
-     55, [(b'custom-key', b'custom-header')]),
+     [('400a 6375 7374 6f6d 2d6b 6579 0d63 7573'
+       '746f 6d2d 6865 6164 6572',
+       [(b'custom-key', b'custom-header')],
+       55, [(b'custom-key', b'custom-header')]),
+      ]),
     ('D.2.2',
-     '040c 2f73 616d 706c 652f 7061 7468',
-     [(b':path', b'/sample/path')],
-     0, []),
+     [('040c 2f73 616d 706c 652f 7061 7468',
+       [(b':path', b'/sample/path')],
+       0, []),
+      ]),
     ('D.2.3',
-     '1008 7061 7373 776f 7264 0673 6563 7265'
-     '74',
-     [(b'password', b'secret')],
-     0, []),
+     [('1008 7061 7373 776f 7264 0673 6563 7265'
+       '74',
+       [(b'password', b'secret')],
+       0, []),
+      ]),
     ('D.2.4',
-     '82',
-     [(b':method', b'GET')],
-     42, [(b':method', b'GET')]),
+     [('82',
+       [(b':method', b'GET')],
+       42, [(b':method', b'GET')]),
+      ]),
     # TODO: add D.2.5 test (header table size limit)
+    ('D.3',
+     [('8287 8644 0f77 7777 2e65 7861 6d70 6c65'
+       '2e63 6f6d',
+       [(b':method', b'GET'),
+        (b':scheme', b'http'),
+        (b':path', b'/'),
+        (b':authority', b'www.example.com')],
+       180, [(b':authority', b'www.example.com'),
+             (b':path', b'/'),
+             (b':scheme', b'http'),
+             (b':method', b'GET')]),
+      ('5c08 6e6f 2d63 6163 6865',
+       [(b'cache-control', b'no-cache'),
+        (b':method', b'GET'),
+        (b':scheme', b'http'),
+        (b':path', b'/'),
+        (b':authority', b'www.example.com')],
+       233, [(b'cache-control', b'no-cache'),
+             (b':authority', b'www.example.com'),
+             (b':path', b'/'),
+             (b':scheme', b'http'),
+             (b':method', b'GET')]),
+      ('3085 8c8b 8440 0a63 7573 746f 6d2d 6b65'
+       '790c 6375 7374 6f6d 2d76 616c 7565',
+       [(b':method', b'GET'),
+        (b':scheme', b'https'),
+        (b':path', b'/index.html'),
+        (b':authority', b'www.example.com'),
+        (b'custom-key', b'custom-value')],
+       379, [(b'custom-key', b'custom-value'),
+             (b':path', b'/index.html'),
+             (b':scheme', b'https'),
+             (b'cache-control', b'no-cache'),
+             (b':authority', b'www.example.com'),
+             (b':path', b'/'),
+             (b':scheme', b'http'),
+             (b':method', b'GET')]),
+      ]),
+    # D.4 encodes the same headers as D.3 but with huffman encoding.
+    ('D.4',
+     [('8287 8644 8cf1 e3c2 e5f2 3a6b a0ab 90f4'
+       'ff',
+       [(b':authority', b'www.example.com'),
+        (b':method', b'GET'),
+        (b':path', b'/'),
+        (b':scheme', b'http')],
+       180, [(b':authority', b'www.example.com'),
+             (b':path', b'/'),
+             (b':scheme', b'http'),
+             (b':method', b'GET')]),
+      ('5c86 a8eb 1064 9cbf',
+       [(b':authority', b'www.example.com'),
+        (b':method', b'GET'),
+        (b':path', b'/'),
+        (b':scheme', b'http'),
+        (b'cache-control', b'no-cache')],
+       233, [(b'cache-control', b'no-cache'),
+             (b':authority', b'www.example.com'),
+             (b':path', b'/'),
+             (b':scheme', b'http'),
+             (b':method', b'GET')]),
+     ]),
 ]
 
 def unhex_test_data(data):
@@ -32,18 +98,19 @@ def unhex_test_data(data):
 
 class HpackDecoderTest(unittest.TestCase):
     def test_hpack_decoder(self):
-        for (name, data, expected, expected_header_table_size,
-             expected_header_table) in test_data:
-            try:
-                decoder = HpackDecoder()
-                result = decoder.decode(unhex_test_data(data))
-                self.assertEqual(result, expected)
-                if expected_header_table_size is not None:
-                    self.assertEqual(decoder._header_table_size,
-                                     expected_header_table_size)
-                if expected_header_table is not None:
-                    self.assertEqual(decoder._header_table[1:],
-                                     expected_header_table)
-            except Exception:
-                print('error in test case', name)
-                raise
+        for name, requests in test_data:
+            decoder = HpackDecoder()
+            for i, (data, expected, expected_header_table_size,
+                    expected_header_table) in enumerate(requests):
+                try:
+                    result = decoder.decode(unhex_test_data(data))
+                    self.assertEqual(sorted(result), sorted(expected))
+                    if expected_header_table_size is not None:
+                        self.assertEqual(decoder._header_table_size,
+                                         expected_header_table_size)
+                    if expected_header_table is not None:
+                        self.assertEqual(list(decoder._header_table),
+                                         expected_header_table)
+                except Exception:
+                    print('error in test case %s, request %d' % (name, i))
+                    raise
